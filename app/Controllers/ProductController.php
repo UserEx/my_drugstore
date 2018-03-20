d84491f0225ed6f7870c14edf63ab2fdede70819
ildar2x6@gmail.com <ildar2x6@gmail.com>
2018-03-21 03:32:00 +1000

    Изменение и удаление товаров. список товаров.

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -17,7 +17,12 @@ class ProductController extends Controller
      */
     public function listAction (Request $request)
     {
-        return new Response($this->view('product_list.html.twig'));
+        $em = $this->container['em'];
+        
+        $products = $em->getRepository('UserEx\MyDrugstore\Entities\Product')
+            ->findAll();
+        
+        return new Response($this->view('product_list.html.twig', array('products' => $products)));
     }
     
     /**
@@ -27,58 +32,7 @@ class ProductController extends Controller
      */
     public function newAction(Request $request)
     {
-        return new Response($this->view('product_new.html.twig', array()));
-    }
-    
-    /**
-     * @param Request $request
-     *
-     * @return \UserEx\MyDrugstore\Core\Response
-     */
-    public function addAction(Request $request)
-    {
-        $product = new Product();
-        $invalidFeedback = array();
-        
-        if ($name = $request->getPost('name')) {
-            $product->setName($name);
-        } else {
-            $invalidFeedback['name'] = 'Укажите название продукта';
-        }
-        
-        if ($price = $request->getPost('price')) {
-            if (is_numeric($price)) {
-                $product->setPrice($price);
-            } else {
-                $invalidFeedback['price'] = 'Укажите корректную цену продукта' . $price;
-            }
-            
-        } else {
-            $invalidFeedback['price'] = 'Укажите цену продукта';
-        }
-        
-        if ($invalidFeedback) {
-            error_log('debud: addAction and invalid_feedback!', E_USER_NOTICE);
-            $response = new Response($this->view('product_new.html.twig', array('invalid_feedback' => $invalidFeedback)));
-        } else {
-            $product->setDescription($request->getPost('description'));
-            $product->setProducingCountry($request->getPost('producing_country'));
-            $product->setManufacturer($request->getPost('manufacturer'));
-            
-            if ($date = $request->getPost('expiry_date')) {
-                $product->setExpiryDate(\DateTime::createFromFormat('Y-m-d', $date));
-            }
-            
-            $router = $this->container['router'];
-            $em = $this->container['em'];
-            
-            $em->getRepository('UserEx\MyDrugstore\Entities\Product')
-                ->add($product);
-            
-            $response = new RedirectResponse($router->getUrl('product_view') . '?id=' . $product->getId());
-        }
-        
-        return $response;        
+        return new Response($this->view('product_edit.html.twig', array('action' => 'add')));
     }
     
     /**
@@ -103,4 +57,127 @@ class ProductController extends Controller
         
         return new Response($this->view('product_view.html.twig', array('product' => $product)));
     }
+    
+    /**
+     * @param Request $request
+     *
+     * @return \UserEx\MyDrugstore\Core\Response
+     */
+    public function editAction(Request $request)
+    {
+        $id = $request->getQuery('id');
+        
+        if ($id) {
+            $em = $this->container['em'];
+            
+            $product = $em->getRepository('UserEx\MyDrugstore\Entities\Product')
+                ->find($id);
+        }
+        
+        if (!$id || !$product) {
+            throw new NotFoundException();
+        }
+        
+        return new Response($this->view('product_edit.html.twig', array('product' => $product, 'action' => 'edit')));
+    }
+    
+    /**
+     * @param Request $request
+     *
+     * @return \UserEx\MyDrugstore\Core\Response
+     */
+    public function saveAction(Request $request)
+    {
+        $id = $request->getPost('id');
+        $action = $request->getPost('action');
+        
+        if ($action == 'edit' && $id) {
+            $em = $this->container['em'];
+            
+            $product = $em->getRepository('UserEx\MyDrugstore\Entities\Product')
+                ->find($id);
+        } elseif ($action == 'add') {
+            $product = new Product();
+        }
+        
+        if ($action == 'edit' && !$product) {
+            throw new NotFoundException();
+        }
+       
+        $invalidFeedback = array();
+        
+        $product->setName($request->getPost('name'));
+        if (!$product->getName()) {
+            $invalidFeedback['name'] = 'Укажите название продукта';
+        }
+        
+        if ($price = $request->getPost('price')) {
+            if (!is_numeric($price)) {
+                $invalidFeedback['price'] = 'Укажите корректную цену продукта' . $price;
+            }
+        } else {
+            $invalidFeedback['price'] = 'Укажите цену продукта';
+        }
+        
+        $product->setPrice($price);
+        $product->setDescription($request->getPost('description'));
+        $product->setProducingCountry($request->getPost('producing_country'));
+        $product->setManufacturer($request->getPost('manufacturer'));
+        
+        if ($date = $request->getPost('expiry_date')) {
+            $experyDate = \DateTime::createFromFormat('Y-m-d', $date);
+        }
+        $product->setExpiryDate($experyDate);
+        
+        if ($invalidFeedback) {
+            error_log('debud: addAction and invalid_feedback!', E_USER_NOTICE);
+            $response = new Response($this->view('product_edit.html.twig', array('product' => $product, 'invalid_feedback' => $invalidFeedback)));
+        } else {            
+            $router = $this->container['router'];
+            $em = $this->container['em'];
+            
+            if ($action == 'add') {
+                $em->getRepository('UserEx\MyDrugstore\Entities\Product')
+                    ->add($product);
+            } elseif ($action == 'edit') {
+                $em->flush();
+            }
+            $response = new RedirectResponse($router->getUrl('product_view') . '?id=' . $product->getId());
+        }
+        
+        return $response;
+    }
+    
+    /**
+     * @param Request $request
+     *
+     * @return \UserEx\MyDrugstore\Core\Response
+     */
+    public function deleteAction(Request $request)
+    {
+        $id = $request->getQuery('id');
+        
+        if ($id) {
+            $em = $this->container['em'];
+            
+            $product = $em->getRepository('UserEx\MyDrugstore\Entities\Product')
+                ->find($id);
+        }
+        
+        error_log('debug: deleteAction!', E_USER_NOTICE);
+        
+        if (!$product) {
+            throw new NotFoundException();
+        }
+        
+        $em->remove($product);
+        $em->flush();
+        
+        $router = $this->container['router'];
+                
+        return new RedirectResponse($router->getUrl('hello_world'));
+    }
+    
+    
+    
 }
